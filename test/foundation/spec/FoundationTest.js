7888af0d627f4144b6244d5a1d43dfee188a0b55
udogerhards <Udo.Gerhards@gerhards.eu>
2019-01-01 19:00:25 +0100

    Added xml based context processing

--- test/foundation/spec/FoundationTest.js
+++ test/foundation/spec/FoundationTest.js
@@ -6,71 +6,8 @@ var assert = require("chai").assert
     , path = require("path")
     , util = require("util");
 
-/**
- * Created by udogerhards on 20.10.18.
- */
 
-describe("XML context described annotation test suite", function(){
-
-    var timeout = 50000;
-
-    var resourcesPath = process.env.PWD.replace("/test","") + path.sep + path.join("lib","foundation");
-    var loggerConfig = process.env.PWD + path.sep + path.join("config", "log4js.json");
-    //var bootstrap = require(process.env.PWD.replace("/test","") + path.sep + path.join("lib", "bootstrap", "bootstrap.js"));
-    //var factory = require(process.env.PWD.replace("/test","") + path.sep + path.join("lib", "factory", "Factory.js"));
-    //var contextBuilder = require(process.env.PWD.replace("/test","") + path.sep + path.join("lib", "context", "ContextBuilder.js"));
-
-    //log4js.configure(loggerConfig);
-    var logger = log4js.getLogger("contextBuilder");
-
-
-    /*
-    it('Should match the correct module dir', function() {
-
-        var foundationClassPath = path.join(resourcesPath, "FoundationClass.js");
-        var foundationInstance = require(foundationClassPath);
-
-        var testDir = "/Users/udogerhards/Documents/Bitbucket/DIAnnotations";
-
-        assert.equal(foundationInstance._moduleDir, testDir, "Module directory not matched");
-    });
-
-
-    /**
-    it('Should test the loading of the foundation configuration', function() {
-
-        var foundationClassPath = path.join(resourcesPath, "FoundationClass.js");
-        var foundationInstance = require(foundationClassPath);
-
-        foundationInstance.init();
-        /*
-            Bootstrap the context and run the tests
-        */
-    /**
-        this.timeout(timeout);
-
-        return new Promise(function(resolve, reject){
-
-            foundationInstance._configuration.done(function(value){
-
-                //console.log(util.inspect(value,{depth:25}));
-
-            });
-        });
-    });
-    */
-
-    it('Should instantiate a XMLContextBuilder instance and test it', function() {
-
-        var xmlContextBuilderClassPath = path.join(resourcesPath,"context", "XMLContextBuilder.js");
-        var xmlContextBuilderClass = require(xmlContextBuilderClassPath);
-
-        var XMLBuilderInstance = new xmlContextBuilderClass();
-        XMLBuilderInstance.init("/Users/udogerhards/Documents/Bitbucket/nodejs-annotations", "/lib/foundation/configuration/xmlContext.xml");
-
-        this.timeout(timeout);
-
-        var res = XMLBuilderInstance.process();
+    var testContext = function(result) {
 
         var objects = [
             "LogManager",
@@ -115,43 +52,91 @@ describe("XML context described annotation test suite", function(){
             "domain"
         ];
 
+        // Check all expected objects
+        objects.forEach(function(value) {
+            assert.isNotNull(result[value], +value+" is null ...");
+            assert.isObject(result[value], value+" is not an object ... ");
+        });
+
+        // Lists
+        functions.forEach(function(value) {
+            assert.isNotNull(result[value], +value+" is null ...");
+            assert.isFunction(result[value], value+" is not a function ... ");
+        });
+
+        // Arrays
+        lists.forEach(function(value) {
+            assert.isNotNull(result[value], +value+" is null ...");
+            assert.isArray(result[value], value+" is not an array ... ");
+        });
+
+        // Check properties
+        properties.forEach(function(value){
+
+            var instance = result[value];
+
+            var properties=Object.getOwnPropertyNames(result[value]);
+
+            properties.forEach(function (prop) {
+                if (!excludeProperties.includes(prop)) {
+                    assert.isNotNull(instance[prop], value + "#" + prop + " is null where it should not!");
+                }
+            });
+
+        });
+
+
+
+    };
+
+/**
+ * Created by udogerhards on 20.10.18.
+ */
+
+describe("XML context described annotation test suite", function(){
+
+    var timeout = 50000;
+
+    var resourcesPath = process.env.PWD.replace("/test","") + path.sep + path.join("lib","foundation");
+    var loggerConfig = process.env.PWD + path.sep + path.join("config", "log4js.json");
+    var logger = log4js.getLogger("contextBuilder");
+
+
+    it('Should instantiate a XMLContextBuilder instance and test it', function() {
+
+        var xmlContextBuilderClassPath = path.join(resourcesPath,"context", "XMLContextBuilder.js");
+        var xmlContextBuilderClass = require(xmlContextBuilderClassPath);
+
+        var XMLBuilderInstance = new xmlContextBuilderClass();
+        XMLBuilderInstance.init("/Users/udogerhards/Documents/Bitbucket/nodejs-annotations", "/lib/foundation/configuration/xmlContext.xml");
+
+        this.timeout(timeout);
+
+        var res = XMLBuilderInstance.process();
+
         return res.then(function(result) {
 
-            // Check all expected objects
-            objects.forEach(function(value) {
-                assert.isNotNull(result[value], +value+" is null ...");
-                assert.isObject(result[value], value+" is not an object ... ");
-            });
-
-            // Lists
-            functions.forEach(function(value) {
-                assert.isNotNull(result[value], +value+" is null ...");
-                assert.isFunction(result[value], value+" is not a function ... ");
-            });
-
-            // Arrays
-            lists.forEach(function(value) {
-                assert.isNotNull(result[value], +value+" is null ...");
-                assert.isArray(result[value], value+" is not an array ... ");
-            });
-
-            // Check properties
-            properties.forEach(function(value){
-
-                var instance = result[value];
-
-                var properties=Object.getOwnPropertyNames(result[value]);
-
-                properties.forEach(function (prop) {
-                    if (!excludeProperties.includes(prop)) {
-                        assert.isNotNull(instance[prop], value + "#" + prop + " is null where it should not!");
-                    }
-                });
-
-            });
+            testContext(result);
 
             return result;
             }
         );
+    });
+
+    it('Should instantiate the intial context from the Foundation instance and test it', function() {
+
+        var foundationClassPath = path.join(resourcesPath, "Foundation.js");
+        var FoundationSingleton = require(foundationClassPath);
+
+        FoundationSingleton.init("/lib/foundation/configuration/xmlContext.xml");
+        var context = FoundationSingleton.process();
+
+        return context.then(function(result){
+
+            testContext(result);
+
+            return result;
+        });
+
     });
 });
