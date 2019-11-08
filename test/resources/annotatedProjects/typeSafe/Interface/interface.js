64d7bb761f3b89358327e8448c74453ffb89c6cb
udogerhards <Udo.Gerhards@gerhards.eu>
2019-11-08 16:34:27 +0100

    Cleanup: Corrected file and class names and removed old code (Store.js)

---
+++ test/resources/annotatedProjects/typeSafe/Interface/interface.js
@@ -0,0 +1,25 @@
+
+
+/**
+ * Created by udogerhards on 2019-08-12.
+ *
+ * @Interface();
+ */
+class test {
+
+    /**
+     * @param {Integer}
+     * @param {Integer}
+     * @param {String}
+     */
+    test(){};
+
+    /**
+     * @param {Integer}
+     * @param {Integer}
+     * @param {String}
+     */
+    test2(){};
+}
+
+module.exports = exports = test;
